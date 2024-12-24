6727a7168a7c95375c3006b36a1597043e90e17c
windyaan <windyanurmaretha@gmail.com>
2024-12-24 15:45:53 +0700

    Menambahkan tabel data Dokter

--- app/src/main/java/com/example/myapplication/Data/Entity/Dokter.kt
+++ app/src/main/java/com/example/myapplication/Data/Entity/Dokter.kt
@@ -1,2 +1,15 @@
 package com.example.myapplication.Data.Entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "dokter")
+data class Dokter(
+    @PrimaryKey
+    val id: String,
+    val nama: String,
+    val spesialis: String,
+    val klinik: String,
+    val noHp: String,
+    val jamKerja: String
+)
